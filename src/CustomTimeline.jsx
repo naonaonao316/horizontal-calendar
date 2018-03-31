73815085c8f94a6b719034c8d1315a14aefcf3f4
Nao Takegawa <nao.takegawa316@gmail.com>
2018-03-31 15:06:11 +0900

    create foundation

--- src/CustomTimeline.jsx
+++ src/CustomTimeline.jsx
@@ -1,34 +1,135 @@
 import React, { Component } from "react";
 import moment from "moment";
+import Timeline from 'react-calendar-timeline/lib';
+import generateFakeData from "./generate-fake-data";
+import { Modal, Button } from "react-bootstrap";
 
-import Timeline from "react-calendar-timeline";
+var keys = {
+  groupIdKey: "id",
+  groupTitleKey: "title",
+  groupRightTitleKey: "rightTitle",
+  itemIdKey: "id",
+  itemTitleKey: "title",
+  itemDivTitleKey: "title",
+  itemGroupKey: "group",
+  itemTimeStartKey: "start",
+  itemTimeEndKey: "end"
+};
 
 export default class CustomTimeline extends Component {
   constructor(props) {
+    super(props);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    const { groups, items } = generateFakeData();
+    const defaultTimeStart = moment()
+      .startOf("day")
+      .toDate();
+    const defaultTimeEnd = moment()
+      .startOf("day")
+      .add(1, "day")
+      .toDate();
+
+    this.state = {
+      groups,
+      items,
+      defaultTimeStart,
+      defaultTimeEnd,
+      show: false
+    };
+  }
+
+  handleItemMove = (itemId, dragTime, newGroupOrder) => {
+    const { items, groups } = this.state;
+
+    const group = groups[newGroupOrder];
+
+    this.setState({
+      items: items.map(
+        item =>
+          item.id === itemId
+            ? Object.assign({}, item, {
+                start: dragTime,
+                end: dragTime + (item.end - item.start),
+                group: group.id
+              })
+            : item
+      )
+    });
+
+    console.log("Moved", itemId, dragTime, newGroupOrder);
+  };
+
+  handleItemResize = (itemId, time, edge) => {
+    const { items } = this.state;
+
+    this.setState({
+      items: items.map(
+        item =>
+          item.id === itemId
+            ? Object.assign({}, item, {
+                start: edge === "left" ? time : item.start,
+                end: edge === "left" ? item.end : time
+              })
+            : item
+      )
+    });
+
+    console.log("Resized", itemId, time, edge);
+  };
+
+  handleCanvasDoubleClick = (group, time, e) => {
+    console.log("canvas gets double clicked!!");
+    this.setState({ show: true });
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
   }
 
   render() {
-    return (
-      <Timeline
-        groups={groups}
-        items={items}
-        keys={keys}
-        fullUpdate
-        sidebarContent={<div>Above The Left</div>}
-        itemsSorted
-        itemTouchSendsClick={false}
-        stackItems
-        itemHeightRatio={0.75}
-        showCursorLine
-        canMove={true}
-        canResize={true}
-        defaultTimeStart={defaultTimeStart}
-        defaultTimeEnd={defaultTimeEnd}
-        onItemMove={this.handleItemMove}
-        onItemResize={this.handleItemResize}
-        onTimeChange={this.handleTimeChange}
-      />
+    const { groups, items, defaultTimeStart, defaultTimeEnd } = this.state;
 
+    return (
+      <div>
+        <Timeline
+          groups={groups}
+          items={items}
+          keys={keys}
+          fullUpdate
+          sidebarContent={<div>Above The Left</div>}
+          itemsSorted
+          itemTouchSendsClick={false}
+          stackItems
+          itemHeightRatio={0.75}
+          showCursorLine
+          canMove={true}
+          canResize={true}
+          defaultTimeStart={defaultTimeStart}
+          defaultTimeEnd={defaultTimeEnd}
+          onItemMove={this.handleItemMove}
+          onItemResize={this.handleItemResize}
+          onTimeChange={this.handleTimeChange}
+          onCanvasDoubleClick={this.handleCanvasDoubleClick}
+        />
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Text in a modal</h4>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 }
